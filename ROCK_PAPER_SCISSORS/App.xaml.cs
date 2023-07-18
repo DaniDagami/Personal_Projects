5b125c5babb8f1ee17017c6a85970876c0404a8c
DaniDagami_Lap <daniel.g.boychev@gmail.com>
2023-07-18 18:13:43 +0300

    yes

---
+++ ROCK_PAPER_SCISSORS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ROCK_PAPER_SCISSORS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
